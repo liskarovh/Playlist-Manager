7039b1b629e3dadbd7e3e78e38b81f5f62ac6e5f
Kacálek Jakub (260263) <xkacalj00@vutbr.cz>
2025-03-03 16:09:54 +0000

    Merged PR 3: Task: add initial migration
    - created DbContext
    - created db factories
    
    Related work items: #9

---
+++ PlaylistManager/PlaylistManager.DAL/Factories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PlaylistManager.DAL.Factories;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PlaylistManagerDbContext>
+{
+    private readonly DbContextSqLiteFactory _dbContextSqLiteFactory = new("playlistmanager.db");
+
+    public PlaylistManagerDbContext CreateDbContext(string[] args) => _dbContextSqLiteFactory.CreateDbContext();
+}
