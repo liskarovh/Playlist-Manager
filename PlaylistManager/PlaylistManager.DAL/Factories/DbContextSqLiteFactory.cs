8660bc67639d36d713c482df019b4aefb38452fe
Jan Kalina <xkalinj00@vutbr.cz>
2025-03-06 21:31:56 +0100

    refactor(DAL): applied code cleanup

--- PlaylistManager/PlaylistManager.DAL/Factories/DbContextSqLiteFactory.cs
+++ PlaylistManager/PlaylistManager.DAL/Factories/DbContextSqLiteFactory.cs
@@ -2,20 +2,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PlaylistManager.DAL.Factories;
 
-public class DbContextSqLiteFactory: IDbContextFactory<PlaylistManagerDbContext>
+public class DbContextSqLiteFactory : IDbContextFactory<PlaylistManagerDbContext>
 {
     private readonly DbContextOptionsBuilder<PlaylistManagerDbContext> _contextOptionsBuilder = new();
     public PlaylistManagerDbContext CreateDbContext() => new(_contextOptionsBuilder.Options);
 
 
     public DbContextSqLiteFactory(string databaseName)
-    {
-        ////May be helpful for ad-hoc testing, not drop in replacement, needs some more configuration.
-        //builder.UseSqlite($"Data Source =:memory:;");
-        _contextOptionsBuilder.UseSqlite($"Data Source={databaseName};Cache=Shared");
-
-        ////Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
-        //_contextOptionsBuilder.EnableSensitiveDataLogging();
-        //_contextOptionsBuilder.LogTo(Console.WriteLine);
-    }
+        =>
+            ////May be helpful for ad-hoc testing, not drop in replacement, needs some more configuration.
+            //builder.UseSqlite($"Data Source =:memory:;");
+            _contextOptionsBuilder.UseSqlite($"Data Source={databaseName};Cache=Shared");
+    ////Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
+    //_contextOptionsBuilder.EnableSensitiveDataLogging();
+    //_contextOptionsBuilder.LogTo(Console.WriteLine);
 }
