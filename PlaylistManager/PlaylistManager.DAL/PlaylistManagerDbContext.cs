7e72887369100fd115a5f3c6d34329174d90de4a
Jan Kalina <xkalinj00@vutbr.cz>
2025-03-06 21:33:20 +0100

    chore(DAL.DbContext): reflects division of PlaylistEntity

--- PlaylistManager/PlaylistManager.DAL/PlaylistManagerDbContext.cs
+++ PlaylistManager/PlaylistManager.DAL/PlaylistManagerDbContext.cs
@@ -4,15 +4,58 @@ using PlaylistManager.DAL.Entities;
 
 namespace PlaylistManager.DAL;
 
+/// <summary>
+/// Represents the database context for the Playlist Manager application.
+/// </summary>
 public class PlaylistManagerDbContext(DbContextOptions contextOptions) : DbContext(contextOptions)
 {
-    public DbSet<PlaylistEntity> Playlists => Set<PlaylistEntity>();
+    /// <summary>
+    /// Gets or sets the DbSet for PlaylistMultimediaEntity.
+    /// </summary>
     public DbSet<PlaylistMultimediaEntity> PlaylistMultimedia => Set<PlaylistMultimediaEntity>();
 
+    /// <summary>
+    /// Gets or sets the DbSet for PlaylistBaseEntity.
+    /// </summary>
+    public DbSet<PlaylistBaseEntity> Playlists => Set<PlaylistBaseEntity>();
+
+    /// <summary>
+    /// Gets or sets the DbSet for MusicPlaylistEntity.
+    /// </summary>
+    public DbSet<MusicPlaylistEntity> MusicPlaylists => Set<MusicPlaylistEntity>();
+
+    /// <summary>
+    /// Gets or sets the DbSet for VideoPlaylistEntity.
+    /// </summary>
+    public DbSet<VideoPlaylistEntity> VideoPlaylists => Set<VideoPlaylistEntity>();
+
+    /// <summary>
+    /// Gets or sets the DbSet for AudioBookPlaylistEntity.
+    /// </summary>
+    public DbSet<AudioBookPlaylistEntity> AudioBookPlaylists => Set<AudioBookPlaylistEntity>();
+
+    /// <summary>
+    /// Gets or sets the DbSet for MultimediaBaseEntity.
+    /// </summary>
     public DbSet<MultimediaBaseEntity> MultimediaBaseEntities => Set<MultimediaBaseEntity>();
+
+    /// <summary>
+    /// Gets or sets the DbSet for AudioMediaEntity.
+    /// </summary>
     public DbSet<AudioMediaEntity> AudioMedia => Set<AudioMediaEntity>();
+
+    /// <summary>
+    /// Gets or sets the DbSet for AudioBookEntity.
+    /// </summary>
     public DbSet<AudioBookEntity> AudioBooks => Set<AudioBookEntity>();
+
+    /// <summary>
+    /// Gets or sets the DbSet for VideoMediaEntity.
+    /// </summary>
     public DbSet<VideoMediaEntity> VideoMedia => Set<VideoMediaEntity>();
+
+    /// <summary>
+    /// Gets or sets the DbSet for MusicEntity.
+    /// </summary>
     public DbSet<MusicEntity> Music => Set<MusicEntity>();
 }
-
