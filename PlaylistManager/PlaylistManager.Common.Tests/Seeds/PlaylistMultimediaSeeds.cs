e2caebb29f55b4692278f54b04935ea712b22cd4
Jan Kalina <xkalinj00@vutbr.cz>
2025-03-07 22:32:47 +0100

    feet(Common.Tests.Seeds): added `PlaylistEntity` and `PlaylistMultimediaEntity` seeds

---
+++ PlaylistManager/PlaylistManager.Common.Tests/Seeds/PlaylistMultimediaSeeds.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlaylistManager.DAL.Entities;
+
+namespace PlaylistManager.Common.Tests.Seeds;
+
+/// <summary>
+/// Provides seed data for PlaylistMultimedia entities.
+/// </summary>
+public static class PlaylistMultimediaSeeds
+{
+    /// <summary>
+    /// Represents an empty PlaylistMultimedia entity.
+    /// </summary>
+    public static readonly PlaylistMultimediaEntity EmptyPlaylistMultimedia = new()
+    {
+        Id = default,
+        PlaylistId = default,
+        MultimediaId = default,
+        Playlist = default!,
+        Multimedia = default!
+    };
+
+    /// <summary>
+    /// Represents the Bohemian Rhapsody multimedia in the Music playlist.
+    /// </summary>
+    public static readonly PlaylistMultimediaEntity MusicPlaylist_BohemianRhapsody = new()
+    {
+        Id = Guid.Parse("40b4f1e8-5605-4c3e-8f4c-c3cb105094a7"),
+        PlaylistId = PlaylistSeeds.MusicPlaylist.Id,
+        Playlist = PlaylistSeeds.MusicPlaylist,
+        MultimediaId = MusicSeeds.BohemianRhapsody.Id,
+        Multimedia = MusicSeeds.BohemianRhapsody
+    };
+
+    /// <summary>
+    /// Represents the American Idiot multimedia in the Music playlist.
+    /// </summary>
+    public static readonly PlaylistMultimediaEntity MusicPlaylist_AmericanIdiot = new()
+    {
+        Id = Guid.Parse("77e70961-178e-4ce6-b6a3-94bb9f7475ea"),
+        PlaylistId = PlaylistSeeds.MusicPlaylist.Id,
+        Playlist = PlaylistSeeds.MusicPlaylist,
+        MultimediaId = MusicSeeds.AmericanIdiot.Id,
+        Multimedia = MusicSeeds.AmericanIdiot
+    };
+
+    /// <summary>
+    /// Represents The Matrix multimedia in the Video playlist.
+    /// </summary>
+    public static readonly PlaylistMultimediaEntity VideoPlaylist_TheMatrix = new()
+    {
+        Id = Guid.Parse("a77defd1-8bc4-455e-bc4f-4a3add1ae820"),
+        PlaylistId = PlaylistSeeds.VideoPlaylist.Id,
+        Playlist = PlaylistSeeds.VideoPlaylist,
+        MultimediaId = VideoMediaSeeds.Matrix.Id,
+        Multimedia = VideoMediaSeeds.Matrix
+    };
+
+    /// <summary>
+    /// Represents the Dune audiobook in the Audiobook playlist.
+    /// </summary>
+    public static readonly PlaylistMultimediaEntity AudioBookPlaylist_Dune = new()
+    {
+        Id = Guid.Parse("8b09152d-a5ad-4cc6-a5da-7f8f3e1784cc"),
+        PlaylistId = PlaylistSeeds.AudioBookPlaylist.Id,
+        Playlist = PlaylistSeeds.AudioBookPlaylist,
+        MultimediaId = AudioBookSeeds.Dune.Id,
+        Multimedia = AudioBookSeeds.Dune
+    };
+
+    /// <summary>
+    /// Represents an updated Bohemian Rhapsody multimedia in the Music playlist.
+    /// </summary>
+    public static readonly PlaylistMultimediaEntity MusicPlaylist_BohemianRhapsodyUpdate =
+        MusicPlaylist_BohemianRhapsody with
+        {
+            Id = Guid.Parse("87f6a5cf-519b-462d-9872-3d17f1c1b67b"),
+            PlaylistId = PlaylistSeeds.MusicPlaylistUpdate.Id,
+            Playlist = PlaylistSeeds.MusicPlaylistUpdate
+        };
+
+    /// <summary>
+    /// Represents a deleted Bohemian Rhapsody multimedia in the Music playlist.
+    /// </summary>
+    public static readonly PlaylistMultimediaEntity MusicPlaylist_BohemianRhapsodyDelete =
+        MusicPlaylist_BohemianRhapsody with
+        {
+            Id = Guid.Parse("d660ede9-513c-44e9-9390-71dca1aeadd7"),
+            PlaylistId = PlaylistSeeds.MusicPlaylistDelete.Id,
+            Playlist = PlaylistSeeds.MusicPlaylistDelete
+        };
+
+    /// <summary>
+    /// Static constructor to initialize the PlaylistMultimediaSeeds class.
+    /// Adds predefined PlaylistMultimedia entities to their respective playlists.
+    /// </summary>
+    static PlaylistMultimediaSeeds()
+    {
+        PlaylistSeeds.MusicPlaylist
+                     .PlaylistMultimedia.Add(MusicPlaylist_BohemianRhapsody);
+
+        PlaylistSeeds.MusicPlaylist
+                     .PlaylistMultimedia.Add(MusicPlaylist_AmericanIdiot);
+
+        PlaylistSeeds.MusicPlaylistUpdate
+                     .PlaylistMultimedia.Add(MusicPlaylist_BohemianRhapsodyUpdate);
+
+        PlaylistSeeds.MusicPlaylistDelete
+                     .PlaylistMultimedia.Add(MusicPlaylist_BohemianRhapsodyDelete);
+
+        PlaylistSeeds.VideoPlaylist
+                     .PlaylistMultimedia.Add(VideoPlaylist_TheMatrix);
+
+        PlaylistSeeds.AudioBookPlaylist
+                     .PlaylistMultimedia.Add(AudioBookPlaylist_Dune);
+    }
+
+    /// <summary>
+    /// Seeds the PlaylistMultimedia entities into the specified DbContext.
+    /// </summary>
+    /// <param name="dbContext">The DbContext to seed.</param>
+    /// <returns>The seeded DbContext.</returns>
+    public static DbContext SeedPlaylistMultimedia(this DbContext dbContext)
+    {
+        dbContext.Set<PlaylistMultimediaEntity>()
+                 .AddRange(EmptyPlaylistMultimedia,
+                           MusicPlaylist_BohemianRhapsody,
+                           MusicPlaylist_AmericanIdiot,
+                           VideoPlaylist_TheMatrix,
+                           AudioBookPlaylist_Dune,
+                           MusicPlaylist_BohemianRhapsodyUpdate,
+                           MusicPlaylist_BohemianRhapsodyDelete
+                          );
+        return dbContext;
+    }
+}
---
+++ PlaylistManager/PlaylistManager.Common.Tests/Seeds/PlaylistSeeds.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlaylistManager.DAL.Entities;
+using PlaylistManager.Common.Enums;
+
+namespace PlaylistManager.Common.Tests.Seeds;
+
+public static class PlaylistSeeds
+{
+    /// <summary>
+    /// Represents an empty Playlist entity.
+    /// </summary>
+    public static readonly PlaylistEntity EmptyPlaylist = new()
+    {
+        Id = default,
+        Title = default!,
+        Description = default,
+        Type = default!,
+        PlaylistMultimedia = default!
+    };
+
+    /// <summary>
+    /// Represents a Playlist entity for rock music.
+    /// </summary>
+    public static readonly PlaylistEntity MusicPlaylist = new()
+    {
+        Id = Guid.Parse("dfcdc163-a6fb-4edb-80b9-1de8f1461a85"),
+        Title = "Rock Playlist",
+        Description = "A collection of rock songs",
+        Type = PlaylistType.Music
+    };
+
+    /// <summary>
+    /// Represents a Playlist entity for favorite movies.
+    /// </summary>
+    public static readonly PlaylistEntity VideoPlaylist = new()
+    {
+        Id = Guid.Parse("78f505ba-ba72-4d84-8e8d-a08e29182192"),
+        Title = "Favorite Movies",
+        Description = "List of favorite sci-fi movies",
+        Type = PlaylistType.Video
+    };
+
+    /// <summary>
+    /// Represents a Playlist entity for fantasy audiobooks.
+    /// </summary>
+    public static readonly PlaylistEntity AudioBookPlaylist = new()
+    {
+        Id = Guid.Parse("341f5161-841e-498d-974c-a2cea40a3c79"),
+        Title = "Fantasy Audiobooks",
+        Description = "Audiobook journey through fantasy worlds",
+        Type = PlaylistType.AudioBook
+    };
+
+    /// <summary>
+    /// Represents an updated clone of the MusicPlaylist entity.
+    /// </summary>
+    public static readonly PlaylistEntity MusicPlaylistUpdate = MusicPlaylist with
+    {
+        Id = Guid.Parse("41b93013-7605-4ee1-bbfb-e1503f125825"),
+        Title = "Rock Playlist Updated",
+        Description = "Updated best of rock 2025"
+    };
+
+    /// <summary>
+    /// Represents a clone of the MusicPlaylist entity for deletion.
+    /// </summary>
+    public static readonly PlaylistEntity MusicPlaylistDelete = MusicPlaylist with
+    {
+        Id = Guid.Parse("df7fa7e8-df17-4f61-b93d-189eae0cbdc6"),
+        Title = "Rock Playlist for Delete"
+    };
+
+    /// <summary>
+    /// Seeds the database context with Playlist entities.
+    /// </summary>
+    /// <param name="dbx">The database context to seed.</param>
+    /// <returns>The seeded database context.</returns>
+    public static DbContext SeedPlaylist(this DbContext dbx)
+    {
+        dbx.Set<PlaylistEntity>()
+           .AddRange(EmptyPlaylist,
+                     AudioBookPlaylist,
+                     VideoPlaylist,
+                     MusicPlaylist,
+                     MusicPlaylistUpdate,
+                     MusicPlaylistDelete
+                    );
+        return dbx;
+    }
+}
