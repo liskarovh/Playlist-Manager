c86bd7c44ad3f204b1225323692d52e8cacc7f8c
Jan Kalina <xkalinj00@vutbr.cz>
2025-03-07 14:18:02 +0100

    chore(Common.Tests): added DeepAssert and XUnitTestOutputConverter

---
+++ PlaylistManager/PlaylistManager.Common.Tests/DeepAssert.cs
@@ -0,0 +1,72 @@
+﻿using KellermanSoftware.CompareNetObjects;
+using Xunit.Sdk;
+
+namespace PlaylistManager.Common.Tests;
+
+/// <summary>
+/// Poskytuje metody pro hluboké porovnání objektů.
+/// </summary>
+public static class DeepAssert
+{
+    /// <summary>
+    /// Porovná dva objekty a vyhodí výjimku, pokud nejsou stejné.
+    /// </summary>
+    /// <typeparam name="T">Typ porovnávaných objektů.</typeparam>
+    /// <param name="expected">Očekávaný objekt.</param>
+    /// <param name="actual">Skutečný objekt.</param>
+    /// <param name="propertiesToIgnore">Vlastnosti, které mají být ignorovány při porovnání.</param>
+    /// <exception cref="EqualException">Vyhozena, pokud objekty nejsou stejné.</exception>
+    public static void Equal<T>(T? expected, T? actual, params string[] propertiesToIgnore)
+    {
+        CompareLogic compareLogic = new()
+        {
+            Config =
+            {
+                MembersToIgnore = propertiesToIgnore.ToList(),
+                IgnoreCollectionOrder = true,
+                IgnoreObjectTypes = true,
+                CompareStaticProperties = false,
+                CompareStaticFields = false
+            }
+        };
+
+        ComparisonResult comparisonResult = compareLogic.Compare(expected!, actual!);
+
+        if (!comparisonResult.AreEqual)
+        {
+            throw EqualException.ForMismatchedValues(expected, actual, comparisonResult.DifferencesString);
+        }
+    }
+
+    /// <summary>
+    /// Zkontroluje, zda kolekce obsahuje očekávaný objekt a vyhodí výjimku, pokud ne.
+    /// </summary>
+    /// <typeparam name="T">Typ porovnávaných objektů.</typeparam>
+    /// <param name="expected">Očekávaný objekt.</param>
+    /// <param name="collection">Kolekce, ve které se má hledat očekávaný objekt.</param>
+    /// <param name="propertiesToIgnore">Vlastnosti, které mají být ignorovány při porovnání.</param>
+    /// <exception cref="ArgumentNullException">Vyhozena, pokud je kolekce null.</exception>
+    /// <exception cref="ContainsException">Vyhozena, pokud kolekce neobsahuje očekávaný objekt.</exception>
+    public static void Contains<T>(T? expected, IEnumerable<T>? collection, params string[] propertiesToIgnore)
+    {
+        if (collection is null)
+            throw new ArgumentNullException(nameof(collection));
+
+        CompareLogic compareLogic = new()
+        {
+            Config =
+            {
+                MembersToIgnore = propertiesToIgnore.ToList(),
+                IgnoreCollectionOrder = true,
+                IgnoreObjectTypes = true,
+                CompareStaticProperties = false,
+                CompareStaticFields = false
+            }
+        };
+
+        if (!collection.Any(item => compareLogic.Compare(expected!, item).AreEqual))
+        {
+            throw ContainsException.ForCollectionItemNotFound(expected!.ToString()!, nameof(collection));
+        }
+    }
+}
---
+++ PlaylistManager/PlaylistManager.Common.Tests/XUnitTestOutputConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Xunit.Abstractions;
+
+namespace PlaylistManager.Common.Tests;
+
+/// <summary>
+/// Třída pro převod výstupu testů XUnit na textový výstup.
+/// </summary>
+public class XUnitTestOutputConverter(ITestOutputHelper output) : TextWriter
+{
+    /// <summary>
+    /// Získává kódování používané textovým zapisovačem.
+    /// </summary>
+    public override Encoding Encoding => Encoding.UTF8;
+
+    /// <summary>
+    /// Zapíše řádek zprávy do výstupu testu.
+    /// </summary>
+    /// <param name="message">Zpráva k zápisu.</param>
+    public override void WriteLine(string? message) => output.WriteLine(message);
+
+    /// <summary>
+    /// Zapíše formátovaný řádek zprávy do výstupu testu.
+    /// </summary>
+    /// <param name="format">Formátovací řetězec.</param>
+    /// <param name="args">Argumenty pro formátovací řetězec.</param>
+    public override void WriteLine(string format, params object?[] args) => output.WriteLine(format, args);
+}
