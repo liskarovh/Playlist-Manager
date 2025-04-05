3dc696ce0645bab2cc6ce85ec37ba9021216e0dd
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-04-05 13:47:37 +0200

    feat(BL.Mappers): created `IModelMapper`, which is a BL mapper interface

---
+++ PlaylistManager/PlaylistManager.BL/Mappers/IModelMapper.cs
@@ -0,0 +1,12 @@
+﻿namespace PlaylistManager.BL.Mappers;
+
+public interface IModelMapper<TEntity, out TNameOnlyModel, out TSummaryModel, TDetailModel>
+{
+    TNameOnlyModel MapToNameOnly(TEntity? entity);
+    IEnumerable<TNameOnlyModel> MapToNameOnly(IEnumerable<TEntity> entities) => entities.Select(MapToNameOnly);
+
+    TSummaryModel MapToSummary(TEntity entity);
+
+    TDetailModel MapToDetailModel(TEntity entity);
+    TEntity MapToEntity(TDetailModel model);
+}
