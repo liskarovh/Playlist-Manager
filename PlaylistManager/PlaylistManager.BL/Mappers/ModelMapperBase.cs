2fa403dac8f3349d40e0e4cc7f067792274ad3ca
Jan Kalina <xkalinj00@vutbr.cz>
2025-04-08 11:30:35 +0000

    Merged PR 32: Task: Create BL.Mappers.Interfaces and BL.Mappers
    - feat(BL.Mappers): implemented `PlaylistModelMapper`, including the logic to count the number of media in the playlist and their total duration.
    - fix(BL.Models): property `PlaylistType` of `PlaylistSummaryModel` can't be nullable because of playlist entity implementation
    - feat(BL.Mappers): implemented `MediumModelMapper` including the logic to choose between media types when mapping, and the logic to bind medium with a playlist via `PlaylistMultimediaEntity`
    - feat(BL.Mappers): created abstract (base) BL model mapper `ModelMapperBase`
    - feat(BL.Mappers): created `IModelMapper`, which is a BL mapper interface
    
    Related work items: #25, #27

---
+++ PlaylistManager/PlaylistManager.BL/Mappers/ModelMapperBase.cs
@@ -0,0 +1,13 @@
+﻿namespace PlaylistManager.BL.Mappers;
+
+public abstract class ModelMapperBase<TEntity, TNameOnlyModel, TSummaryModel, TDetailModel>
+    : IModelMapper<TEntity, TNameOnlyModel, TSummaryModel, TDetailModel>
+{
+    public abstract TNameOnlyModel MapToNameOnly(TEntity? entity);
+
+    public virtual IEnumerable<TNameOnlyModel> MapToNameOnly(IEnumerable<TEntity> entities) => entities.Select(MapToNameOnly);
+
+    public abstract TSummaryModel MapToSummary(TEntity entity);
+    public abstract TDetailModel MapToDetailModel(TEntity entity);
+    public abstract TEntity MapToEntity(TDetailModel model);
+}
