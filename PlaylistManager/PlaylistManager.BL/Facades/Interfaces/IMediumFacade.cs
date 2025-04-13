d39b27dcfb78ec6e1f23463d7786afaabe682d8f
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-04-13 23:18:45 +0200

    refactor(BL.Facades): applied format and clean-up, applied rename refactoring

--- PlaylistManager/PlaylistManager.BL/Facades/Interfaces/IMediumFacade.cs
+++ PlaylistManager/PlaylistManager.BL/Facades/Interfaces/IMediumFacade.cs
@@ -1,10 +1,6 @@
-// Copyright (c).NET Foundation and contributors.All rights reserved.
-// Licensed under the MIT license.See LICENSE file in the project root for full license information.
-
 using PlaylistManager.BL.Models;
 using PlaylistManager.DAL.Entities;
+
 namespace PlaylistManager.BL.Facades.Interfaces;
 
-public interface IMediumFacade : IFacade<PlaylistMultimediaEntity,MediumNameOnlyModel, MediumSummaryModel, MediumDetailedModel>
-{
-}
+public interface IMediumFacade : IFacade<PlaylistMultimediaEntity, MediumNameOnlyModel, MediumSummaryModel, MediumDetailedModel> { }
--- PlaylistManager/PlaylistManager.BL/Facades/PlaylistFacade.cs
+++ PlaylistManager/PlaylistManager.BL/Facades/PlaylistFacade.cs
@@ -1,19 +1,16 @@
-using PlaylistManager.BL.Facades.Interfaces;
-using PlaylistManager.BL.Mappers;
-using PlaylistManager.BL.Models;
 using PlaylistManager.DAL.Entities;
 using PlaylistManager.DAL.Mappers;
 using PlaylistManager.DAL.UnitOfWork;
+using PlaylistManager.BL.Facades.Interfaces;
+using PlaylistManager.BL.Mappers;
+using PlaylistManager.BL.Models;
 
 namespace PlaylistManager.BL.Facades;
 
-public class PlaylistFacade(
-    IUnitOfWorkFactory unitOfWorkFactory,
-    PlaylistModelMapper modelMapper)
-    : FacadeBase<PlaylistEntity, PlaylistNameOnlyModel, PlaylistSummaryModel, PlaylistSummaryModel, PlaylistEntityMapper>(unitOfWorkFactory,
-        modelMapper), IPlaylistFacade
+public class PlaylistFacade(IUnitOfWorkFactory unitOfWorkFactory, PlaylistModelMapper modelMapper)
+    : FacadeBase<PlaylistEntity, PlaylistNameOnlyModel, PlaylistSummaryModel, PlaylistSummaryModel, PlaylistEntityMapper>(unitOfWorkFactory, modelMapper),
+      IPlaylistFacade
 {
-    protected override ICollection<string> IncludesNavigationPathDetail =>
-        new[] {$"{nameof(PlaylistEntity.PlaylistMultimedia)}.{nameof(PlaylistMultimediaEntity.Multimedia)}"};
-
+    protected override ICollection<string> IncludesNavigationPathDetail
+        => [$"{nameof(PlaylistEntity.PlaylistMultimedia)}.{nameof(PlaylistMultimediaEntity.Multimedia)}"];
 }
