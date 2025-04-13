22bb5a7be76c5dfaaeaa429521231496bad8cc0a
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-04-13 14:35:11 +0200

    style(BL.Facades.MediumFacade): code cleanup
    Signed-off-by: Jan Kalina <xkalinj00@stud.fit.vutbr.cz>

--- PlaylistManager/PlaylistManager.BL/Facades/MediumFacade.cs
+++ PlaylistManager/PlaylistManager.BL/Facades/MediumFacade.cs
@@ -7,24 +7,7 @@ using PlaylistManager.DAL.UnitOfWork;
 
 namespace PlaylistManager.BL.Facades;
 
-
-// public class PlaylistFacade(
-//     IUnitOfWorkFactory unitOfWorkFactory,
-//     PlaylistModelMapper modelMapper)
-//     : FacadeBase<PlaylistEntity, PlaylistNameOnlyModel, PlaylistSummaryModel, PlaylistSummaryModel, PlaylistEntityMapper>(unitOfWorkFactory,
-//         modelMapper), IPlaylistFacade
-// {
-//     protected override ICollection<string> IncludesNavigationPathDetail =>
-//         new[] {$"{nameof(PlaylistEntity.PlaylistMultimedia)}.{nameof(PlaylistMultimediaEntity.Multimedia)}"};
-//
-// }
-
-
-public class MediumFacade(
-    IUnitOfWorkFactory unitOfWorkFactory,
-    MediumModelMapper modelMapper)
-    :
-        FacadeBase<PlaylistMultimediaEntity, MediumNameOnlyModel,MediumSummaryModel, MediumDetailedModel, PlaylistMultimediaEntityMapper>(
-            unitOfWorkFactory, modelMapper), IMediumFacade
-{
-}
+public class MediumFacade(IUnitOfWorkFactory unitOfWorkFactory,
+                          MediumModelMapper modelMapper)
+    : FacadeBase<PlaylistMultimediaEntity, MediumNameOnlyModel, MediumSummaryModel, MediumDetailedModel, PlaylistMultimediaEntityMapper>(unitOfWorkFactory, modelMapper),
+      IMediumFacade { }
