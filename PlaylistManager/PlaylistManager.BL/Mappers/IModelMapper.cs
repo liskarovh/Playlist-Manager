89d9d01f2e1362f0238ec7d4fa6495a9b0b90f4f
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-04-13 23:20:13 +0200

    refactor(BL.Mappers): applied format and clean-up
    Signed-off-by: Jan Kalina <xkalinj00@stud.fit.vutbr.cz>

--- PlaylistManager/PlaylistManager.BL/Mappers/IModelMapper.cs
+++ PlaylistManager/PlaylistManager.BL/Mappers/IModelMapper.cs
@@ -3,11 +3,12 @@
 public interface IModelMapper<TEntity, out TNameOnlyModel, out TSummaryModel, TDetailModel>
 {
     TNameOnlyModel MapToNameOnly(TEntity? entity);
-    IEnumerable<TNameOnlyModel> MapToNameOnly(IEnumerable<TEntity> entities) => entities.Select(MapToNameOnly);
+    IEnumerable<TNameOnlyModel> MapToNameOnly(IEnumerable<TEntity> entities)
+        => entities.Select(MapToNameOnly);
 
     TSummaryModel MapToSummary(TEntity entity);
-    IEnumerable<TSummaryModel> MapToSummary(IEnumerable<TEntity> entities) => entities.Select(MapToSummary);
-
+    IEnumerable<TSummaryModel> MapToSummary(IEnumerable<TEntity> entities)
+        => entities.Select(MapToSummary);
 
     TDetailModel MapToDetailModel(TEntity entity);
     TEntity MapToEntity(TDetailModel model);
--- PlaylistManager/PlaylistManager.BL/Mappers/ModelMapperBase.cs
+++ PlaylistManager/PlaylistManager.BL/Mappers/ModelMapperBase.cs
@@ -5,10 +5,12 @@ public abstract class ModelMapperBase<TEntity, TNameOnlyModel, TSummaryModel, TD
 {
     public abstract TNameOnlyModel MapToNameOnly(TEntity? entity);
 
-    public virtual IEnumerable<TNameOnlyModel> MapToNameOnly(IEnumerable<TEntity> entities) => entities.Select(MapToNameOnly);
+    public virtual IEnumerable<TNameOnlyModel> MapToNameOnly(IEnumerable<TEntity> entities)
+        => entities.Select(MapToNameOnly);
 
     public abstract TSummaryModel MapToSummary(TEntity entity);
-    public virtual IEnumerable<TSummaryModel> MapToSummary(IEnumerable<TEntity> entities) => entities.Select(MapToSummary);
+    public virtual IEnumerable<TSummaryModel> MapToSummary(IEnumerable<TEntity> entities)
+        => entities.Select(MapToSummary);
 
     public abstract TDetailModel MapToDetailModel(TEntity entity);
     public abstract TEntity MapToEntity(TDetailModel model);
