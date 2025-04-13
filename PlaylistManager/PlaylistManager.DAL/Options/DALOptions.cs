c30a1349e53b3d9a94f3f9e43f1552bb396263d4
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-04-13 23:21:05 +0200

    refactor(DAL.Options): applied format and clean-up
    Signed-off-by: Jan Kalina <xkalinj00@stud.fit.vutbr.cz>

--- PlaylistManager/PlaylistManager.DAL/Options/DALOptions.cs
+++ PlaylistManager/PlaylistManager.DAL/Options/DALOptions.cs
@@ -1,12 +1,9 @@
-// Copyright (c).NET Foundation and contributors.All rights reserved.
-// Licensed under the MIT license.See LICENSE file in the project root for full license information.
-
 namespace PlaylistManager.DAL.Options;
 
 public class DALOptions
 {
-    public string DatabaseDirectory { get; init; } = string.Empty;
-    public string DatabaseName { get; init; } = string.Empty;
+    private string DatabaseDirectory { get; init; } = string.Empty;
+    private string DatabaseName { get; init; } = string.Empty;
     public string DatabaseFilePath => Path.Combine(DatabaseDirectory, DatabaseName);
 
     /// <summary>
@@ -14,8 +11,5 @@ public class DALOptions
     /// </summary>
     public bool RecreateDatabaseEachTime { get; init; } = false;
 
-    /// <summary>
-    /// Seeds DemoData from DbContext on database creation.
-    /// </summary>
     public bool SeedDemoData { get; init; } = false;
 }
