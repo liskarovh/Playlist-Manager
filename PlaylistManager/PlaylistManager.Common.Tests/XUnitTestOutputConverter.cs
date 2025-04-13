f362ab1082f48d0af74491beea0982d6501438af
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-04-13 23:34:57 +0200

    refactor(Common.Tests): applied format and clean-up
    Signed-off-by: Jan Kalina <xkalinj00@stud.fit.vutbr.cz>

--- PlaylistManager/PlaylistManager.Common.Tests/XUnitTestOutputConverter.cs
+++ PlaylistManager/PlaylistManager.Common.Tests/XUnitTestOutputConverter.cs
@@ -3,26 +3,14 @@ using Xunit.Abstractions;
 
 namespace PlaylistManager.Common.Tests;
 
-/// <summary>
-/// Class for converting XUnit test output to text output.
-/// </summary>
 public class XUnitTestOutputConverter(ITestOutputHelper output) : TextWriter
 {
-    /// <summary>
-    /// Gets the encoding used by the text writer.
-    /// </summary>
-    public override Encoding Encoding => Encoding.UTF8;
+    public override Encoding Encoding
+        => Encoding.UTF8;
 
-    /// <summary>
-    /// Writes a line of message to the test output.
-    /// </summary>
-    /// <param name="message">The message to write.</param>
-    public override void WriteLine(string? message) => output.WriteLine(message);
+    public override void WriteLine(string? message)
+        => output.WriteLine(message);
 
-    /// <summary>
-    /// Writes a formatted line of message to the test output.
-    /// </summary>
-    /// <param name="format">The format string.</param>
-    /// <param name="args">The arguments for the format string.</param>
-    public override void WriteLine(string format, params object?[] args) => output.WriteLine(format, args);
+    public override void WriteLine(string format, params object?[] args)
+        => output.WriteLine(format, args);
 }
