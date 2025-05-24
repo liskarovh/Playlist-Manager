4539b8534a9b4d3e5f49ca5e269329c51118c8b4
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2025-05-24 21:03:10 +0200

    chore(App.PlaylistOverviewViewModel): loads only playlists of provided manager type, prepared search/sort/filter operations

--- PlaylistManager/PlaylistManager.App/ViewModels/PlaylistOverviewViewModel.cs
+++ PlaylistManager/PlaylistManager.App/ViewModels/PlaylistOverviewViewModel.cs
@@ -6,6 +6,7 @@ using PlaylistManager.App.Services;
 using PlaylistManager.App.Views;
 using PlaylistManager.BL.Facades.Interfaces;
 using PlaylistManager.BL.Models;
+using PlaylistManager.Common.Enums;
 using PropertyChanged;
 
 namespace PlaylistManager.App.ViewModels;
@@ -14,18 +15,19 @@ namespace PlaylistManager.App.ViewModels;
 public partial class PlaylistOverviewViewModel : ViewModelBase,
                                                  IRecipient<PlaylistAddMessage>,
                                                  IRecipient<PlaylistDeleteMessage>,
-                                                 IRecipient<PlaylistEditMessage>
+                                                 IRecipient<PlaylistEditMessage>,
+                                                 IRecipient<ManagerSelectedMessage>
 {
     private readonly IPlaylistFacade _playlistFacade;
     private readonly INavigationService _navigationService;
+    private ManagerType _selectedManagerType = ManagerType.NotDecided;
 
     public ObservableCollection<PlaylistSummaryModel> Playlists { get; set; } = new();
     public string PlaylistSearchQuery { get; set; } = string.Empty;
     public string MediumSearchQuery { get; set; } = string.Empty;
-    public string SelectedSortOption { get; set; } = string.Empty;
+    public string SelectedSortOption { get; set; } = "Name";
 
     public ObservableCollection<string> SortOptions { get; } = new(["Name", "Media Count", "Total Duration"]);
-
 
     public PlaylistOverviewViewModel(IPlaylistFacade playlistFacade, INavigationService navigationService, IMessengerService messengerService)
         : base(messengerService)
@@ -50,6 +52,12 @@ public partial class PlaylistOverviewViewModel : ViewModelBase,
                            };
 
         WeakReferenceMessenger.Default.RegisterAll(this);
+    }
+
+    public async void Receive(ManagerSelectedMessage message)
+    {
+        _selectedManagerType = message.SelectedType;
+        await LoadDataAsync();
     }
 
     public void Receive(PlaylistAddMessage message)
@@ -78,7 +86,9 @@ public partial class PlaylistOverviewViewModel : ViewModelBase,
 
     private async Task LoadPlaylists()
     {
-        var playlists = await _playlistFacade.GetAsyncSummary();
+        var playlistType = MapManagerTypeToPlaylistType(_selectedManagerType);
+
+        var playlists = await _playlistFacade.GetPlaylistsByTypeAsync(playlistType);
 
         Playlists.Clear();
         foreach (var playlist in playlists)
@@ -94,10 +104,10 @@ public partial class PlaylistOverviewViewModel : ViewModelBase,
 
         IEnumerable<PlaylistSummaryModel> sortedPlaylists = sortOption switch
         {
-            "Název"       => Playlists.OrderBy(p => p.Title),
-            "Počet médií" => Playlists.OrderByDescending(p => p.MediaCount),
-            "Délka"       => Playlists.OrderByDescending(p => p.TotalDuration),
-            _             => Playlists
+            "Name"           => Playlists.OrderBy(p => p.Title),
+            "Media Count"    => Playlists.OrderByDescending(p => p.MediaCount),
+            "Total Duration" => Playlists.OrderByDescending(p => p.TotalDuration),
+            _                => Playlists
         };
 
         var newList = sortedPlaylists.ToList();
@@ -111,19 +121,21 @@ public partial class PlaylistOverviewViewModel : ViewModelBase,
     [RelayCommand]
     private async Task SearchPlaylists(string? searchQuery)
     {
+        IEnumerable<PlaylistSummaryModel> results;
+        var playlistType = MapManagerTypeToPlaylistType(_selectedManagerType);
+
         if (string.IsNullOrEmpty(searchQuery))
         {
-            await LoadPlaylists();
-            return;
+            results = await _playlistFacade.GetPlaylistsByTypeAsync(playlistType);
+        }
+        else
+        {
+            //TODO: results = await _playlistFacade.GetPlaylistsByNameAsync(searchQuery);
+            results = await _playlistFacade.GetAsyncSummary(); // Placeholder
         }
 
-        var filteredPlaylists = Playlists.Where(p =>
-                                                    p.Title.Contains(searchQuery, StringComparison.OrdinalIgnoreCase) ||
-                                                    (p.Description != null && p.Description.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)))
-                                         .ToList();
-
         Playlists.Clear();
-        foreach (var playlist in filteredPlaylists)
+        foreach (var playlist in results)
         {
             Playlists.Add(playlist);
         }
@@ -132,47 +144,44 @@ public partial class PlaylistOverviewViewModel : ViewModelBase,
     [RelayCommand]
     private async Task SearchMedia(string? searchQuery)
     {
+        IEnumerable<PlaylistSummaryModel> results;
+
         if (string.IsNullOrEmpty(searchQuery))
         {
-            await LoadPlaylists();
-            return;
+            results = await _playlistFacade.GetAsyncSummary();
+        }
+        else
+        {
+           //TODO: results = await _playlistFacade.GetPlaylistsByMediaTitleAsync(searchQuery);
+           results = await _playlistFacade.GetAsyncSummary(); // Placeholder
         }
 
-        var filteredPlaylists = Playlists.Where(p =>
-                                                    p.Medias.Any(m => m.Title.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)))
-                                         .ToList();
-
         Playlists.Clear();
-        foreach (var playlist in filteredPlaylists)
+        foreach (var playlist in results)
         {
             Playlists.Add(playlist);
         }
     }
 
     [RelayCommand]
-    private void CreatePlaylist()
+    private async Task CreatePlaylist()
     {
         var newPlaylist = PlaylistSummaryModel.Empty with
         {
             PlaylistId = Guid.NewGuid(),
-            Title = "Nový playlist",
-            Description = "Popis nového playlistu",
+            Type = MapManagerTypeToPlaylistType(_selectedManagerType),
+            Title = "New playlist",
+            Description = "Description of the new playlist",
             MediaCount = 0,
             TotalDuration = 0
+
         };
 
-        MessengerService.Send(new PlaylistAddMessage(newPlaylist));
+        var savedPlaylist = await _playlistFacade.SaveAsync(newPlaylist);
 
-        _navigationService.GoToAsync($"playlist?id={newPlaylist.PlaylistId}");
-    }
+        MessengerService.Send(new PlaylistAddMessage(savedPlaylist));
 
-    [RelayCommand]
-    private async Task DeletePlaylist(PlaylistSummaryModel? playlist)
-    {
-        if (playlist == null) return;
-
-        await _playlistFacade.DeleteAsync(playlist.PlaylistId);
-        MessengerService.Send(new PlaylistDeleteMessage(playlist.PlaylistId.ToString()));
+        //TODO: await _navigationService.GoToAsync($"playlist?id={savedPlaylist.PlaylistId}");
     }
 
     [RelayCommand]
@@ -182,12 +191,23 @@ public partial class PlaylistOverviewViewModel : ViewModelBase,
 
         MessengerService.Send(new PlaylistSelectedMessage(playlist));
 
-        _navigationService.GoToAsync($"playlist?id={playlist.PlaylistId}");
+        //TODO: _navigationService.GoToAsync($"playlist?id={playlist.PlaylistId}");
     }
 
     [RelayCommand]
     private void GoBack()
     {
         _navigationService.GoToAsync("//select");
+    }
+
+    private PlaylistType MapManagerTypeToPlaylistType(ManagerType managerType)
+    {
+        return managerType switch
+        {
+            ManagerType.Video     => PlaylistType.Video,
+            ManagerType.Music     => PlaylistType.Music,
+            ManagerType.AudioBook => PlaylistType.AudioBook,
+            _                     => PlaylistType.Music // Výchozí hodnota pro NotDecided
+        };
     }
 }
