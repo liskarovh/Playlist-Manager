86533bfbfa9b164d2f61d0b70eb9deeb2a732783
Kacálek Jakub (260263) <xkacalj00@vutbr.cz>
2025-05-24 19:21:52 +0000

    Merged PR 44: Created sorting and filtering methods in playlist facade
    Related work items: #32

--- PlaylistManager/PlaylistManager.BL/Facades/Interfaces/IPlaylistFacade.cs
+++ PlaylistManager/PlaylistManager.BL/Facades/Interfaces/IPlaylistFacade.cs
@@ -1,6 +1,20 @@
+using PlaylistManager.BL.Enums;
 using PlaylistManager.BL.Models;
 using PlaylistManager.DAL.Entities;
 
 namespace PlaylistManager.BL.Facades.Interfaces;
 
-public interface IPlaylistFacade : IFacade<PlaylistEntity, PlaylistNameOnlyModel, PlaylistSummaryModel, PlaylistSummaryModel> { }
+public interface
+    IPlaylistFacade : IFacade<PlaylistEntity, PlaylistNameOnlyModel, PlaylistSummaryModel, PlaylistSummaryModel>
+{
+    Task<IEnumerable<PlaylistSummaryModel>> GetPlaylistsByNameAsync(string? namePrefix);
+    Task<IEnumerable<MediumSummaryModel>> GetMediaInPlaylistByTitleAsync(Guid playlistId, string mediaTitlePrefix);
+    Task<IEnumerable<PlaylistSummaryModel>> GetPlaylistsSortedAsync(PlaylistSortBy sortBy, SortOrder sortOrder);
+    Task<IEnumerable<MediumSummaryModel>> GetMediaInPlaylistSortedAsync(Guid playlistId, MediaSortBy sortBy, SortOrder sortOrder);
+
+    Task<IEnumerable<MediumSummaryModel>> GetMediaInPlaylistSortedAsync(
+        Guid playlistId,
+        string? mediaTitlePrefix,
+        MediaSortBy sortBy,
+        SortOrder sortOrder);
+}
