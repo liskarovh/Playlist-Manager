65ac95e08b04697d7dceab20675804a2a984de5c
Kalina Jan (260259) <xkalinj00@vutbr.cz>
2025-05-24 07:34:33 +0000

    Merged PR 41: Prepared Alert, Navigation and Messenger services
    - Prepared the above-mentioned services.
    - Created a RouteModel.
    - Added a few TODOs into the NavigationService to add some simple navigations, when the views are prepared.
    
    Related work items: #36

---
+++ PlaylistManager/PlaylistManager.App/Models/RouteModel.cs
@@ -0,0 +1,3 @@
+﻿namespace PlaylistManager.App.Models;
+
+public record RouteModel(string Route, Type ViewType, Type ViewModelType) { }
---
+++ PlaylistManager/PlaylistManager.App/Services/NavigationService.cs
@@ -0,0 +1,43 @@
+﻿using PlaylistManager.App.Models;
+using PlaylistManager.App.ViewModels;
+using PlaylistManager.App.Views;
+
+namespace PlaylistManager.App.Services;
+
+public class NavigationService : INavigationService
+{
+    public IEnumerable<RouteModel> Routes { get; } = new List<RouteModel>
+    {
+        // TODO: Add  routes here
+        // TODO: new("//select/music", typeof(SelectManagerView), typeof(XXX)),
+        // TODO: new("//select/videos", typeof(SelectManagerView), typeof(XXX)),
+        // TODO: new("//select/audioBooks", typeof(SelectManagerView), typeof(XXX)),
+    };
+
+    public async Task GoToAsync<TViewModel>()
+        where TViewModel : IViewModel
+    {
+        var route = GetRouteByViewModel<TViewModel>();
+        await Shell.Current.GoToAsync(route);
+    }
+
+    public async Task GoToAsync<TViewModel>(IDictionary<string, object?> parameters)
+        where TViewModel : IViewModel
+    {
+        var route = GetRouteByViewModel<TViewModel>();
+        await Shell.Current.GoToAsync(route, parameters);
+    }
+
+    public async Task GoToAsync(string route)
+        => await Shell.Current.GoToAsync(route);
+
+    public async Task GoToAsync(string route, IDictionary<string, object?> parameters)
+        => await Shell.Current.GoToAsync(route, parameters);
+
+    public bool SendBackButtonPressed()
+        => Shell.Current.SendBackButtonPressed();
+
+    private string GetRouteByViewModel<TViewModel>()
+        where TViewModel : IViewModel
+        => Routes.First(route => route.ViewModelType == typeof(TViewModel)).Route;
+}
